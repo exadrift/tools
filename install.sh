fcc6904b3cb787eb95ce213ee9547932942df472
wire-oss <wire-oss@noreply.github.com>
2026-07-04 05:22:25 +0900

    add installer, containerize all builds

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+CONTAINER_NAME=temp-exadrift
+
+if [ "$1" == "" ]
+then
+    echo "install.sh <name> <version>"
+    exit 1
+fi
+IMAGE_NAME=$1
+
+if [ "$2" == "" ]
+then
+    echo "install.sh <name> <version>"
+    exit 1
+fi
+IMAGE_TAG=$2
+
+DOCKER_LOC=$(which docker)
+if [ "${DOCKER_LOC}" == "" ]
+then
+    echo "you must have docker installed in order to user the installer"
+    exit 1
+fi
+
+TARGET_PATH=/usr/local/bin/${IMAGE_NAME}
+
+docker rm -f ${CONTAINER_NAME}
+docker container create --name ${CONTAINER_NAME} exadrift/${IMAGE_NAME}:${IMAGE_TAG}
+docker container cp ${CONTAINER_NAME}:/${IMAGE_NAME} ${TARGET_PATH}
+chmod +x /usr/local/bin/${IMAGE_NAME}
+docker rm -f ${CONTAINER_NAME}
+echo "${IMAGE_NAME} installed at ${TARGET_PATH}"
