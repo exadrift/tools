9ffd636bfe2d3fee69f7579b297659fc501ad191
wire-oss <wire-oss@noreply.github.com>
2026-08-20 10:35:34 +0900

    updated installer

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ ARCH=$(uname -m | sed 's/x86_64/amd64/')
 LATEST_VERSION=$(curl https://raw.githubusercontent.com/exadrift/tools/refs/heads/main/${RELEASE_NAME}/VERSION)
 TARGET_PATH=/usr/local/bin/${RELEASE_NAME}
 
-curl -L https://github.com/exadrift/tools/releases/download/kubex-${LATEST_VERSION}/${RELEASE_NAME}-${OS}-${ARCH} -o ${TARGET_PATH}
+echo "downloading ${RELEASE_NAME} version ${LATEST_VERSION}"
+curl -L https://github.com/exadrift/tools/releases/download/${RELEASE_NAME}-${LATEST_VERSION}/${RELEASE_NAME}-${OS}-${ARCH} -o ${TARGET_PATH}
 chmod +x ${TARGET_PATH}
 
 echo "${RELEASE_NAME} installed at ${TARGET_PATH}"
